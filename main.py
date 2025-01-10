7b6759ecd655ebdf1f579255a22c626f4210b4eb
Joseap1996 <joseap1996@live.com>
2025-01-10 15:49:47 -0600

    D: add print report to main.py

--- main.py
+++ main.py
@@ -3,8 +3,30 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
+    list_of_dict = convert_list(num_characters)
+    list_of_dict.sort(reverse=True, key=sort_on)
+    print("--- Begin report of books/frakenstein.txt ---")
     print(f"{num_words} words found in the document")
-    print(f"{num_characters}")
+    print("")
+    for char_dict in list_of_dict:
+        print(f"The '{char_dict["char"]}' character was found {char_dict["num"]} times" )
+    print("--- End report ---")
+   
+
+def convert_list(num_characters):
+    chars_list = []
+    for char, count in num_characters.items():
+        if char.isalpha():
+            new_dict = {
+                "char": char,
+                "num": count
+            } 
+            chars_list.append(new_dict)
+    return chars_list
+
+def sort_on(dict):
+    return dict["num"]
+
 
 def get_num_characters(text):
     chars = {}
